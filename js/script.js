fc1506d344cc4e64daef3f54f39ea512960a1b5d
Kevin Schmid <schmid.kevin.manuel@gmail.com>
2019-04-27 17:49:48 +0200

    finished rework and cleaned up

--- js/script.js
+++ js/script.js
@@ -1,67 +1,56 @@
 let dataSet = [];
-let worker = new Worker('./js/worker.js');
-worker.onmessage = event => {
-	addData(event.data);
-	redraw()
-}
+let blinkColor = 1;
+let blinkColorAsc = true;
 
-const about = () => {
-	document.getElementById('about').style.display = 'block';
-	document.getElementById('github').style.display = 'none';
-
-	let text = "day";
-	let hours = new Date().getHours();
-	if( hours < 12) {
-		text = "morning";
-	} else if( hours < 16 ) {
-		text = "afternoon";
-	} else {
-		text = "evening";
-	}
-	document.getElementById('time').innerHTML=text;
-}
-
-const github = () => {
-	document.getElementById('about').style.display = 'none';
-	document.getElementById('github').style.display = 'block';
-}
-
-about();
-document.getElementsByClassName('about')[0].addEventListener('click', about, false);
-document.getElementsByClassName('github')[0].addEventListener('click', github, false);
-
-const addData = data => {
-	let date = new Date(data.date);
-	let currKey = (date.getFullYear()-2000) + '-' + (date.getMonth() + 1).toString().padStart(2, '0');
-	let foundData = dataSet.find(entry => entry.key == currKey);
-	if(foundData) {
-		foundData.value = foundData.value + 1;
-	} else {
-		dataSet.push({
-			key: currKey,
-			value: 1
-		});
-	}
-	dataSet.sort((a, b) => a.key.localeCompare( b.key ));
+if (!!window.SharedWorker) {
+	let worker = new SharedWorker('./js/worker.js');
+	worker.port.onmessage = event => dataSet = event.data.dataSet;
+	worker.port.start();
+	worker.port.postMessage('');
 }
 
 const redraw = () => {
 	let c = document.getElementById("github_content");
 	let ctx = c.getContext("2d");
-	let maxX = c.width -20;
-	let maxY = c.height -20;
 	let x = 0;
-	let y = 0;
-
+	let y = 5;
+	const factor = 20;
+	const padding = 2;
+	c.height = dataSet.length * (factor+padding+y);
+	
 	ctx.clearRect(0, 0, c.width, c.height);
+	ctx.font = "20px 'Noto Serif', serif";
+	ctx.fillStyle = '#444444';
+	let blinkDrawn = false;
 	dataSet.forEach(data => {
 		x = 0;
-		ctx.fillText(data.key, x, y+10);
-		x += 28;
+		ctx.fillText(data.key, x, y+factor-padding);
+		x += 60;
 		for(var j = 0; j < data.value; j++) {
-			ctx.fillRect(x, y, 10, 10);
-			x += 12;
+			if(c.width < (x + factor + padding + 20)) {
+				ctx.fillText('...', x, y+factor-padding);
+				break;
+			}
+			ctx.fillRect(x, y, factor, factor);
+			x += factor+padding;
 		}
-		y += 12;
+		if(!blinkDrawn) {
+			let hex = ('0' + blinkColor.toString(16)).substr(-2);
+			ctx.strokeStyle = '#'+hex+hex+hex;
+			ctx.rect(x, y, factor, factor);
+			ctx.stroke();
+			blinkDrawn = true;
+		}
+		y += factor+padding;
 	});
+	if(blinkColor > 254 || blinkColor < 1) {
+		blinkColorAsc = !blinkColorAsc;
+	}
+	if(blinkColorAsc) {
+		blinkColor = blinkColor + 1;
+	} else {
+		blinkColor = blinkColor - 1;
+	}
+	window.requestAnimationFrame(redraw);
 }
+redraw();
--- js/worker.js
+++ js/worker.js
@@ -1,3 +1,37 @@
+let dataSet = [];
+let commitCount = 0;
+let port;
+
+onconnect = function(e) {
+    port = e.ports[0];
+    if(commitCount < 1) {
+        port.onmessage = e => fetchData();
+    }
+}
+
+const fetchData = () => fetch('https://api.github.com/users/CodingGentleman/repos')
+    .then(status)
+    .then(json)
+    .then(data => data.forEach(repo => handleRepo(repo.name)))
+    .catch(error);
+
+const handleRepo = name => {
+    fetch('https://api.github.com/repos/CodingGentleman/'+name+'/commits?author=CodingGentleman')
+    .then(status)
+    .then(json)
+    .then(data => data.forEach(commit => handleCommit(commit)))
+    .catch(error);
+}
+
+const handleCommit = commits => {
+    addData(commits.commit.author.date);
+    commitCount++;
+	port.postMessage({
+        'commitCount': commitCount,
+        'dataSet': dataSet
+    });
+}
+
 const status = response => {
     if (response.status >= 200 && response.status < 300) {
         return Promise.resolve(response)
@@ -9,27 +43,17 @@ const status = response => {
 const json = response => response.json();
 const error = err => console.log('Fetch Error: ', err);
 
-fetch('https://api.github.com/users/CodingGentleman/repos')
-    .then(status)
-    .then(json)
-    .then(function(data) {
-        data.forEach(repo => handleRepo(repo.name));
-    })
-    .catch(error);
-
-function handleRepo(name) {
-    fetch('https://api.github.com/repos/CodingGentleman/'+name+'/commits?author=CodingGentleman')
-    .then(status)
-    .then(json)
-    .then(function(data){
-        data.forEach(commit => handleCommit(commit));
-    })
-    .catch(error);
-}
-
-function handleCommit(commits) {
-	postMessage({
-        'sha': commits.sha,
-        'date': commits.commit.author.date
-    });
+const addData = commitDate => {
+	let date = new Date(commitDate);
+	let currKey = (date.getFullYear()-2000) + '-' + (date.getMonth() + 1).toString().padStart(2, '0');
+	let foundData = dataSet.find(entry => entry.key == currKey);
+	if(foundData) {
+		foundData.value = foundData.value + 1;
+	} else {
+		dataSet.push({
+			key: currKey,
+			value: 1
+		});
+	}
+	dataSet.sort((a, b) => b.key.localeCompare( a.key ));
 }
