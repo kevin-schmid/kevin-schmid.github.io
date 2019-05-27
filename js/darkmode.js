8e68ab4fa6499abc5901ae99e653efe1511b9f9f
Kevin Schmid <schmid.kevin.manuel@gmail.com>
2019-05-27 15:42:26 +0200

    improved responsive design, added darkmode

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,4 +1,5 @@
 // to use enable chrome://flags/#enable-generic-sensor-extra-classes 
+let darkmode = false;
 if ("AmbientLightSensor" in window) {
     try {
         var sensor = new AmbientLightSensor();
@@ -8,5 +9,16 @@ if ("AmbientLightSensor" in window) {
         sensor.start();
     } catch (e) {
         console.error(e);
+    }
+}
+
+const update = lux => {
+    if(lux < 40 ^ darkmode) {
+        darkmode = !darkmode;
+        if(darkmode) {
+            document.documentElement.classList.add("darkmode");
+        } else {
+            document.documentElement.classList.remove("darkmode");
+        }
     }
 }
