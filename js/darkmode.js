467ddd46772a317333e73dc5e766927822afdbff
Kevin Schmid <kevin.schmid@knapp.com>
2020-07-16 15:24:26 +0200

    reduced footprint

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,30 +1,3 @@
-// to use enable chrome://flags/#enable-generic-sensor-extra-classes 
-let darkmode = false;
-if ("AmbientLightSensor" in window) {
-    try {
-        var sensor = new AmbientLightSensor();
-        sensor.addEventListener("reading", function (event) {
-            if(lux < 40 ^ darkmode) {
-                darkmode = !darkmode;
-                switchMode(darkmode);
-            }
-        });
-        sensor.start();
-    } catch (e) {
-        console.error(e);
-    }
-} else {
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-        switchMode(e.matches);
-    });
-}
-
-const switchMode = darkmode => {
-    if(darkmode) {
-        document.documentElement.classList.add("darkmode");
-    } else {
-        document.documentElement.classList.remove("darkmode");
-    }
-}
-
-switchMode(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
+const s = v => v ? document.documentElement.classList.add("darkmode") : document.documentElement.classList.remove("darkmode");
+s(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {s(e.matches)});
