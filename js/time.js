d0718814e865331c3ffa47962925e37feac0af1c
Kevin Schmid <schmid.kevin.manuel@gmail.com>
2021-06-03 21:25:15 +0200

    homepage redesign

--- js/time.js
+++ js/time.js
@@ -2,7 +2,7 @@ let text = "evening";
 let d = new Date();
 if(d.getHours() < 12) {
 	text = "morning";
-} else if( d.getHours() < 16 ) {
+} else if(d.getHours() < 16 ) {
 	text = "afternoon";
 }
 document.getElementById('time').innerHTML=text;
