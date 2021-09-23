3178fba785730a5d1c5a152dafdce1a6474f5361
Kevin Schmid <kevin.schmid@knapp.com>
2021-09-23 14:56:32 +0200

    disabled time dependent greeting for media print, fixed print head padding

--- js/time.js
+++ js/time.js
@@ -1,9 +1,19 @@
-let text = "evening";
-let d = new Date();
-if(d.getHours() < 12) {
-	text = "morning";
-} else if(d.getHours() < 16 ) {
-	text = "afternoon";
+function updateTime(x) {
+	let d = new Date();
+	if(x.matches) {
+		document.getElementById('time').innerHTML = "day";
+	} else {
+		let text = "evening";
+		if(d.getHours() < 12) {
+			text = "morning";
+		} else if(d.getHours() < 16 ) {
+			text = "afternoon";
+		}
+		document.getElementById('time').innerHTML=text;
+	}
+	document.getElementById('year').innerHTML=d.getFullYear();
 }
-document.getElementById('time').innerHTML=text;
-document.getElementById('year').innerHTML=d.getFullYear();
+
+var x = window.matchMedia("print")
+updateTime(x)
+x.addEventListener("change", updateTime)
