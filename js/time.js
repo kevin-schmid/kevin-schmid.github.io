194105766729de395a1ccd125c0c78e007fcf6a9
Kevin Schmid <kevin.schmid@knapp.com>
2021-09-30 09:15:22 +0200

    changed time update to better support printing, also refactoring

--- js/time.js
+++ js/time.js
@@ -1,19 +1,29 @@
-function updateTime(x) {
+updatePartOfDay = () => {
 	let d = new Date();
+	let text = "evening";
+	if(d.getHours() < 12) {
+		text = "morning";
+	} else if(d.getHours() < 16 ) {
+		text = "afternoon";
+	}
+	if(text !== document.getElementById('time').innerHTML) {
+		document.getElementById('time').innerHTML = text;
+	}
+}
+
+document.getElementById('year').innerHTML = new Date().getFullYear();
+updatePartOfDay();
+setInterval(updatePartOfDay, 60*1000);
+
+window.matchMedia("print").addEventListener("change", (x) => {
 	if(x.matches) {
 		document.getElementById('time').innerHTML = "day";
 	} else {
-		let text = "evening";
-		if(d.getHours() < 12) {
-			text = "morning";
-		} else if(d.getHours() < 16 ) {
-			text = "afternoon";
-		}
-		document.getElementById('time').innerHTML=text;
+		updatePartOfDay();
 	}
-	document.getElementById('year').innerHTML=d.getFullYear();
-}
+});
 
-var x = window.matchMedia("print")
-updateTime(x)
-x.addEventListener("change", updateTime)
+window.addEventListener('afterprint', updatePartOfDay);
+window.addEventListener("beforeprint", (event) => {
+	document.getElementById('time').innerHTML = "day";
+});
